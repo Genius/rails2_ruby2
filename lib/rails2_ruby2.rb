444b6952ac66a32ff7b3c6dab674d383cac01a8d
Andrew Warner <wwarner.andrew@gmail.com>
2015-07-20 21:55:39 -0400

    Refactor to gem structure
    - Everything happens in rails_init which gets called as part of the
      rails init process
    - We specifically load rails_init in all gem test suites

--- lib/rails2_ruby2.rb
+++ lib/rails2_ruby2.rb
@@ -4,21 +4,7 @@ require "rails2_ruby2/kernel_helpers"
 in_ruby2 do
   require 'yaml'
   require 'syck'
-end
-
-in_ruby2_when_library_loaded('ActiveSupport', :version_file => 'active_support/version') do
   require 'rails2_ruby2/backport_ruby_config'
-  require 'rails2_ruby2/i18n_patch'
-  require 'rails2_ruby2/slice_patch'
-  require 'rails2_ruby2/tzinfo_patch'
-end
-
-in_ruby2_when_library_loaded('ActionController',
-   :lib_const_string => 'ActionPack', :version_file => 'action_pack/version') do
-  require 'rails2_ruby2/named_route_collection_patch'
-  require 'rails2_ruby2/convert_cookies_patch'
-  require 'rails2_ruby2/filter_parameters_patch'
-  require 'rails2_ruby2/action_controller_perform_action_patch'
 end
 
 module Rails2Ruby2
--- lib/rails2_ruby2/rails_init.rb
+++ lib/rails2_ruby2/rails_init.rb
@@ -1,4 +1,18 @@
-if Rails::VERSION::MAJOR == 2 && RUBY_VERSION >= '2.0.0'
+in_ruby2_when_library_loaded('Rails') do
   require 'rails2_ruby2/active_record_associations'
   require 'rails2_ruby2/rails_generator_patch'
+end
+
+in_ruby2_when_library_loaded('ActiveSupport', :version_file => 'active_support/version') do
+  require 'rails2_ruby2/i18n_patch'
+  require 'rails2_ruby2/slice_patch'
+  require 'rails2_ruby2/tzinfo_patch'
+end
+
+in_ruby2_when_library_loaded('ActionController',
+   :lib_const_string => 'ActionPack', :version_file => 'action_pack/version') do
+  require 'rails2_ruby2/named_route_collection_patch'
+  require 'rails2_ruby2/convert_cookies_patch'
+  require 'rails2_ruby2/filter_parameters_patch'
+  require 'rails2_ruby2/action_controller_perform_action_patch'
 end
