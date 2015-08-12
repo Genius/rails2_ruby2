fc6d3e46ccb8854fa637e07cfc1467526e3bfb2e
Andrew Warner <andrew@rapgenius.com>
2015-08-12 11:11:13 -0400

    Patch another respond_to? call

--- lib/rails2_ruby2/active_record_associations.rb
+++ lib/rails2_ruby2/active_record_associations.rb
@@ -12,3 +12,37 @@ module ActiveRecord
     end
   end
 end
+
+
+module ActiveRecord::AutosaveAssociation
+  private
+
+  def save_collection_association(reflection)
+    if association = association_instance_get(reflection.name)
+      autosave = reflection.options[:autosave]
+
+      if records = associated_records_to_validate_or_save(association, @new_record_before_save, autosave)
+        records.each do |record|
+          next if record.destroyed?
+
+          if autosave && record.marked_for_destruction?
+            association.destroy(record)
+          elsif autosave != false && (@new_record_before_save || record.new_record?)
+            if autosave
+              saved = association.send(:insert_record, record, false, false)
+            else
+              association.send(:insert_record, record)
+            end
+          elsif autosave
+            saved = record.save(false)
+          end
+
+          raise ActiveRecord::Rollback if saved == false
+        end
+      end
+
+      # reconstruct the SQL queries now that we know the owner's id
+      association.send(:construct_sql) if association.respond_to?(:construct_sql, true)
+    end
+  end
+end
