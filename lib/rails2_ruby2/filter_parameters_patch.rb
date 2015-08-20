6b4a704a2b6bb8441a5fd7d251f2ebe7799b74df
Andrew Warner <wwarner.andrew@gmail.com>
2015-08-19 21:54:01 -0400

    Consistently call respond_to? with include_all using "true"

--- lib/rails2_ruby2/filter_parameters_patch.rb
+++ lib/rails2_ruby2/filter_parameters_patch.rb
@@ -2,7 +2,7 @@ ActionController::Base.prepend(Module.new do
   private
 
   def log_processing_for_parameters
-    parameters = respond_to?(:filter_parameters, :include_all) ? filter_parameters(params) : params.dup
+    parameters = respond_to?(:filter_parameters, true) ? filter_parameters(params) : params.dup
     parameters = parameters.except!(:controller, :action, :format, :_method)
 
     logger.info "  Parameters: #{parameters.inspect}" unless parameters.empty?
