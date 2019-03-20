bc7abbd81757004425fbf2d7cbc8c57780c37224
Zach Dennis <zach.dennis@gmail.com>
2019-03-20 14:35:17 -0400

    Removing no longer necessary hash slice path
    This fix was included in the upstream rails 2.3 LTS branch.
    
    Reference: https://github.com/makandra/rails/commit/523a98435c6086efa518bcdb856c84069cacb64c

--- lib/rails2_ruby2/rails_init.rb
+++ lib/rails2_ruby2/rails_init.rb
@@ -1,6 +1,5 @@
 when_library_loaded('ActiveSupport', :version_file => 'active_support/version') do
   require 'rails2_ruby2/i18n_patch'
-  # require 'rails2_ruby2/slice_patch'
   # require 'rails2_ruby2/tzinfo_patch'
   # require 'rails2_ruby2/time_zone_patch'
   require 'rails2_ruby2/compressed_memcached_store_patch'
