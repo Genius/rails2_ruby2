56427ac78bfa5cabef0ead6ebde468de4f3d3be9
Zach Dennis <zach.dennis@gmail.com>
2019-03-20 11:10:35 -0400

    Move rails_init into main rails2_ruby2 rather than rails/init.rb.
    This is to prevent railties tests from failing because it will be automatically loaded by rails as a plugin and there is a test that asserts loaded plugins. This test does not expect rails2_ruby2 to show up.
    
    This may be reverted later, but for now this keeps railties test suite green.
    
    Command run in the rails2 directory to verify:
    
       rbenv local 2.3.8 && pushd railties && bundle exec --gemfile ../Gemfile.2.3 rake test ; popd

--- lib/rails2_ruby2.rb
+++ lib/rails2_ruby2.rb
@@ -1,6 +1,6 @@
 require "rails2_ruby2/version"
-
-# require "rails2_ruby2/kernel_helpers"
+require "rails2_ruby2/kernel_helpers"
+require "rails2_ruby2/rails_init"
 
 # require 'yaml'
 # require 'syck'
--- rails/init.rb
+++
@@ -1,1 +0,0 @@
-require File.expand_path('../../lib/rails2_ruby2/rails_init', __FILE__)
