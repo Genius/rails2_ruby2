f641e4f1c16189ee9a91171b18c85504c7ed9f3b
Zach Dennis <zach.dennis@gmail.com>
2019-03-20 15:51:00 -0400

    Adding back in the erb util silence patch
    This is still necessary in Rails 2.3 LTS.

--- lib/rails2_ruby2/rails_init.rb
+++ lib/rails2_ruby2/rails_init.rb
@@ -1,7 +1,7 @@
 when_library_loaded('ActiveSupport', :version_file => 'active_support/version') do
   require 'rails2_ruby2/i18n_patch'
   require 'rails2_ruby2/compressed_memcached_store_patch'
-  # require 'rails2_ruby2/erb_util_silence'
+  require 'rails2_ruby2/erb_util_silence'
 end
 
 when_library_loaded('ActionController',
