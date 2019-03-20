60976d2693f0e6f016eddbc041c53386a9686bbf
Zach Dennis <zach.dennis@gmail.com>
2019-03-20 16:21:53 -0400

    Adding back in backport ruby config patch
    There are still dependencies which refer to Config::CONFIG rather than RbConfig::CONFIG.
    
    You can do a rudimentary check by going into vendor/bundle/gems and running:
    
        grep -ER "\bConfig::CONFIG\b" *
    
    NOTE:
    
    This does not affect Rails, but gem dependencies of the Rails app.

--- lib/rails2_ruby2.rb
+++ lib/rails2_ruby2.rb
@@ -2,7 +2,7 @@ require "rails2_ruby2/version"
 require "rails2_ruby2/kernel_helpers"
 require "rails2_ruby2/rails_init"
 
-# require 'rails2_ruby2/backport_ruby_config'
+require 'rails2_ruby2/backport_ruby_config'
 
 module Rails2Ruby2
 end
