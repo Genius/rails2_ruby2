f6f4271991aa42709c955f18d377551a7966f97d
Zach Dennis <zach.dennis@gmail.com>
2019-03-20 10:59:46 -0400

    Comment out all monkey patches. Will add back in as necessary.

--- lib/rails2_ruby2.rb
+++ lib/rails2_ruby2.rb
@@ -1,10 +1,11 @@
 require "rails2_ruby2/version"
-require "rails2_ruby2/kernel_helpers"
 
-require 'yaml'
-require 'syck'
-require 'rails2_ruby2/deserialize_ordered_hashes'
-require 'rails2_ruby2/backport_ruby_config'
+# require "rails2_ruby2/kernel_helpers"
+
+# require 'yaml'
+# require 'syck'
+# require 'rails2_ruby2/deserialize_ordered_hashes'
+# require 'rails2_ruby2/backport_ruby_config'
 
 module Rails2Ruby2
 end
