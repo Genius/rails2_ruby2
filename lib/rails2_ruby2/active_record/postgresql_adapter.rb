58505fbbc1eaf58053f335eda774b40ac0769d0b
Nate Kandler <natekandler@gmail.com>
2019-03-26 10:38:30 -0700

    adds ActiveRecord patch dump_schema_information

---
+++ lib/rails2_ruby2/active_record/postgresql_adapter.rb
@@ -0,0 +1,7 @@
+ActiveRecord::ConnectionAdapters::SchemaStatements.module_eval do
+  def dump_schema_information #:nodoc:
+    sm_table = "public.#{ActiveRecord::Migrator.schema_migrations_table_name}"
+    migrated = select_values("SELECT version FROM #{sm_table} ORDER BY version")
+    migrated.map { |v| "INSERT INTO #{sm_table} (version) VALUES ('#{v}');" }.join("\n\n")
+  end
+end
