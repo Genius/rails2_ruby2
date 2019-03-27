7e66adaec09d99c3942d0f8006bd158c0e80a2f6
Zach Dennis <zach.dennis@gmail.com>
2019-03-27 09:52:47 -0400

    Use simpler String#chomp to remove file extension rather than sub/Regexp

--- lib/rails2_ruby2/action_pack.rb
+++ lib/rails2_ruby2/action_pack.rb
@@ -1,3 +1,3 @@
 Dir[File.dirname(__FILE__) + '/action_pack/**/*.rb'].each do |file|
-  require file.sub(/#{Regexp.escape(File.extname(file))}$/, '')
+  require file.chomp('.rb')
 end
--- lib/rails2_ruby2/active_record.rb
+++ lib/rails2_ruby2/active_record.rb
@@ -1,3 +1,3 @@
 Dir[File.dirname(__FILE__) + '/active_record/**/*.rb'].each do |file|
-  require file.sub(/#{Regexp.escape(File.extname(file))}$/, '')
+  require file.chomp('.rb')
 end
