4bc95f546254d11350246ece29c0312d9ddf43f7
Nate Kandler <natekandler@gmail.com>
2019-04-01 10:00:12 -0700

    add patch to support default values that are negative integers

---
+++ lib/rails2_ruby2/active_record/postgresql_column.rb
@@ -0,0 +1,15 @@
+require 'active_record/connection_adapters/postgresql_adapter'
+
+ActiveRecord::ConnectionAdapters::PostgreSQLColumn.class_eval do
+  class << self
+    def extract_value_from_default_with_negative_integer_support(default)
+      if default =~ /\A'(-?\d+(\.\d*)?\)?)'::integer\z/i
+        $1
+      else
+        extract_value_from_default_without_negative_integer_support
+      end
+    end
+
+    alias_method_chain :extract_value_from_default, :negative_integer_support
+  end
+end
